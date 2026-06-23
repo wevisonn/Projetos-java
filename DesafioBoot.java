68798ec8731dd5fb09f9bc4a2e5842a27ceb7157
wevisonn <wevison4@gmail.com>
2026-06-22 21:24:34 -0300

    Create DesafioBoot.java

---
+++ DesafioBoot.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DesafioBoot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Qual seu saldo? ");
+        int saldo = scanner.nextInt();
+      System.out.print("Qual o valor da transação? ");  
+        int valorTransacao = scanner.nextInt();
+
+        System.out.println((saldo >= valorTransacao) ? "Transacao aprovada" : "Saldo insuficiente" );
+        scanner.close();
+    }
+}
